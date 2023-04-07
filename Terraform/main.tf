a989dc9f1bb4e9c47a34126d1df3101f1ada4b51
Irina Geiman <irina.geiman@opsguru.io>
2023-04-07 00:14:15 -0400

    Deployment works, application is functional

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -47,7 +47,7 @@ module "parameters" {
 module "alb" {
   source              = "./alb"
   vpc_id              = module.vpc.vpc_id
-  aws_launch_template = module.ec2.aws_launch_template
+  aws_launch_template = module.ec2.aws_launch_template.id
   public_subnets      = module.vpc.public_subnets
 
   depends_on = [
--- Terraform/rds/main.tf
+++ Terraform/rds/main.tf
@@ -33,7 +33,7 @@ resource "aws_db_instance" "this_rds" {
   publicly_accessible     = false
   db_subnet_group_name    = aws_db_subnet_group.this.name
   vpc_security_group_ids  = [aws_security_group.this_db_sg.id]
-  name                    = var.db_paramters["database"]
+  db_name                 = var.db_paramters["database"]
   username                = var.db_paramters["username"]
   password                = var.db_paramters["password"]
   backup_retention_period = 7
